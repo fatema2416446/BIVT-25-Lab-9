b61f6194654c8f7715ffa6f97e6908d79d0368d3
fatema2416446 <m2416446@edu.misis.ru>
2026-05-18 10:39:27 +0300

    Update White.cs

--- Lab9/White/White.cs
+++ Lab9/White/White.cs
@@ -1,3 +1,22 @@
 ﻿namespace Lab9.White
 {
+ public abstract class White
+
+    private string _input;
+
+    public string Input => _input;
+
+    protected White(string text)
+    {
+        _input = text ?? string.Empty;
+    }
+
+    public abstract void Review();
+
+    public virtual void ChangeText(string text)
+    {
+        _input = text ?? string.Empty;
+        Review();
+    }
 }
+
